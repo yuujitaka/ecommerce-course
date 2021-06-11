cd9978b86680222a9cac34799cd842cc0b43489c
Cleviane <rebecacleviane@gmail.com>
2021-06-11 16:22:49 -0300

    cart com redux e reselect

--- src/components/cart-dropdown/cart-dropdown.component.jsx
+++ src/components/cart-dropdown/cart-dropdown.component.jsx
@@ -1,13 +1,27 @@
 import React from 'react';
 import CustomButton from '../custom-button/custom-button.component';
+import CartItem from '../cart-item/cart-item.component';
+import { connect } from 'react-redux';
+import { selectCartItems } from '../../redux/cart/cart.selectors';
+
 
 import './cart-dropdown.styles.scss';
 
-const CartDropdown = () => (
+const CartDropdown = ({ cartItemsList }) => (
   <div className='cart-dropdown'>
-    <div className='cart-items'/>
+    <div className='cart-items'>
+      { 
+        cartItemsList.map(cartItem => (
+           <CartItem key={cartItem.id} item={cartItem}/>
+        ))
+      }
+    </div>
     <CustomButton>CHECKOUT</CustomButton>
   </div>
 )
 
-export default CartDropdown;
+const mapStateToProps = state => ({
+  cartItemsList: selectCartItems(state)
+});
+
+export default connect(mapStateToProps)(CartDropdown);
--- src/components/cart-icon/cart-icon.component.jsx
+++ src/components/cart-icon/cart-icon.component.jsx
@@ -1,19 +1,28 @@
 import React from 'react';
 import { connect } from 'react-redux';
 import { toggleVisibility } from '../../redux/cart/cart.actions';
+import { selectCartItemsCount } from '../../redux/cart/cart.selectors';
+
+
 import { ReactComponent as ShoppingIcon } from '../../assets/images/shopping-bag.svg';
 import './cart-icon.styles.scss';
 
 
-const CartIcon = ({ toggleCart }) => (
-  <div className='cart-icon' onClick={toggleCart}>
-    <ShoppingIcon className='shopping-icon'/>
-    <span className='item-count'>0</span>
-  </div>
-)
+const CartIcon = ({ toggleCart, cartItemsListCount }) => {
+  return(
+    <div className='cart-icon' onClick={toggleCart}>
+      <ShoppingIcon className='shopping-icon'/>
+      <span className='item-count'>{cartItemsListCount}</span>
+    </div>
+  )
+}
 
 const mapDispatchToProps = dispatch => ({
   toggleCart: () => dispatch(toggleVisibility())
 });
 
-export default connect(null,mapDispatchToProps)(CartIcon);
+const mapStateToProps = state => ({
+  cartItemsListCount: selectCartItemsCount(state)
+});
+
+export default connect(mapStateToProps,mapDispatchToProps)(CartIcon);
